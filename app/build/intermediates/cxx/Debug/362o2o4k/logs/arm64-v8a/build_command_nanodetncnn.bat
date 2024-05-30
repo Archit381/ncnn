8cdf199bce49c6e46739e5617fd13b3160eeca89
Archit <architojha420@gmail.com>
2024-05-31 01:34:26 +0530

    first

---
+++ app/build/intermediates/cxx/Debug/362o2o4k/logs/arm64-v8a/build_command_nanodetncnn.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Archit\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Archit\\Desktop\\ncnn\\ncnn-android-nanodet\\app\\.cxx\\Debug\\362o2o4k\\arm64-v8a" ^
+  nanodetncnn
